50cbb4061eafca552c238ce8c5b227a87736e50a
Youelmm <yelmesaoudim@gmail.com>
2023-01-04 22:00:32 +0100

    Added the string representation of a binary tree and the possibility to get its nodes

--- binarytree.py
+++ binarytree.py
@@ -2,6 +2,22 @@ class BinaryTree():
     
     def __init__(self, root=None):
         self.root = root
+    
+    def __str__(self):
+        nodes_to_handle = []
+        current_node = self.root
+        nodes_to_handle.append(current_node)
+        representation = ""
+        while len(nodes_to_handle) != 0:
+            representation += f"{current_node.left_node} <-- {current_node} --> {current_node.right_node}\n"
+            if current_node.left_node != None:
+                nodes_to_handle.append(current_node.left_node)
+            if current_node.right_node != None:
+                nodes_to_handle.append(current_node.right_node)
+            nodes_to_handle.pop(0)
+            if len(nodes_to_handle) != 0:
+                current_node = nodes_to_handle[0]
+        return representation
 
     def contains(self, node) -> bool:
         current_node = self.root
@@ -39,9 +55,11 @@ class BinaryTree():
     
     def get_nodes(self):
         nodes_to_handle = []
+        nodes = []
         current_node = self.root
         nodes_to_handle.append(current_node)
         while len(nodes_to_handle) != 0:
+            nodes.append(current_node)
             print(f"{current_node.left_node} <-- {current_node} --> {current_node.right_node}")
             if current_node.left_node != None:
                 nodes_to_handle.append(current_node.left_node)
@@ -50,3 +68,4 @@ class BinaryTree():
             nodes_to_handle.pop(0)
             if len(nodes_to_handle) != 0:
                 current_node = nodes_to_handle[0]
+        return nodes
